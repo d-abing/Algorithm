db29d35a8128557c00d92e188400d5d00230164c
Dabin Choi <ynlio117@naver.com>
2023-10-24 16:13:25 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1096 ms, Memory: 298712 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+fun main() {
+    var bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+    for (i in 1 .. readLine()!!.toInt()) {
+        var V1: List<String> = readLine()!!.split(" ");
+        bw.write("${V1[0].toInt() + V1[1].toInt()} \n")
+    }
+    bw.flush()
+}
