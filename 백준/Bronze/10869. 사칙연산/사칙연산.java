cc24cedf158d2d44b15465af514a94bf65974c60
Dabin Choi <ynlio117@naver.com>
2023-10-24 16:19:02 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+	}
+}
