afb29f02444c2040888c21f0643a9df10ddd00a2
Dabin Choi <ynlio117@naver.com>
2023-11-07 13:57:20 +0900

    [Bronze II] Title: 음계, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,8 @@
+numbers = list(map(int, input().split()))
+
+if numbers == sorted(numbers):
+    print("ascending")
+elif numbers == sorted(numbers, reverse=True):
+    print("descending")
+else:
+    print("mixed")
