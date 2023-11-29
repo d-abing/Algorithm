09218e3981f859e9d6cee6e736becb6465721790
Dabin Choi <ynlio117@naver.com>
2023-11-29 12:03:08 +0900

    [Silver IV] Title: 큐 2, Time: 964 ms, Memory: 254492 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+queue = deque()
+
+for _ in range(N):
+    order = input().split()
+    if order[0] == 'push':
+        queue.append(order[1])
+    elif order[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif order[0] == 'size':
+        print(len(queue))
+    elif order[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif order[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif order[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
