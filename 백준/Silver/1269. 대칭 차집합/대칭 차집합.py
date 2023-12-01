888274b31edefdc3ab5354f96a43b6b639e959bd
Dabin Choi <ynlio117@naver.com>
2023-12-01 15:53:26 +0900

    [Silver IV] Title: 대칭 차집합, Time: 332 ms, Memory: 209764 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -0,0 +1,7 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+print(len((A.union(B) - (A.intersection(B)))))
