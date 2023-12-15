d4ff278b4a2584baea083bf667e73bfeb8fdeed9
Dabin Choi <ynlio117@naver.com>
2023-12-15 15:46:32 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,11 @@
+school_name = input()
+
+if school_name == "NLCS":
+    print("North London Collegiate School")
+elif school_name == "BHA":
+    print("Branksome Hall Asia")
+elif school_name == "KIS":
+    print("Korea International School")
+elif school_name == "SJA":
+    print("St. Johnsbury Academy")
+    
