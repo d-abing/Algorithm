afedc9d019a0cea35c50c927a37855dde1a9fa83
Dabin Choi <ynlio117@naver.com>
2023-12-15 16:34:08 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,17 @@
+N = int(input())
+for _ in range(N):
+    class_name = input().rstrip()
+    if class_name == "Algorithm":
+        print("204")
+    elif class_name == "DataAnalysis":
+        print("207")
+    elif class_name == "ArtificialIntelligence":
+        print("302")
+    elif class_name == "CyberSecurity":
+        print("B101")
+    elif class_name == "Network":
+        print("303")
+    elif class_name == "Startup":
+        print("501")
+    elif class_name == "TestStrategy":
+        print("105")
