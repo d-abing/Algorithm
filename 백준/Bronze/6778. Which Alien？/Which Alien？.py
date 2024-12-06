1ade0fa0173c0f2d522fdaf37e8b7843fcd532f4
Dabin Choi <ynlio117@naver.com>
2024-12-07 01:35:43 +0900

    [Bronze IV] Title: Which Alien?, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,9 @@
+A = int(input())
+E = int(input())
+
+if A >= 3 and E <= 4:
+    print("TroyMartian")
+if A <= 6 and E >= 2:
+    print("VladSaturnian")
+if A <= 2 and E <= 3:
+    print("GraemeMercurian")
