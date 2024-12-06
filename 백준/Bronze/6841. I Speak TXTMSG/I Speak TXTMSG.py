ff48d3dd224b4ccb8e88f472fe3473c788dbc416
Dabin Choi <ynlio117@naver.com>
2024-12-07 01:58:25 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,52 @@
+while True:
+    M = input()
+
+    if M == "TTYL":
+        print("talk to you later")
+        break
+
+    if M == "CU":
+        print("see you")
+        continue
+
+    if M == ":-)":
+        print("I’m happy")
+        continue
+
+    if M == ":-(":
+        print("I’m unhappy")
+        continue
+
+    if M == ";-)":
+        print("wink")
+        continue
+
+    if M == ":-P":
+        print("stick out my tongue")
+        continue
+
+    if M == "(~.~)":
+        print("sleepy")
+        continue
+
+    if M == "TA":
+        print("totally awesome")
+        continue
+
+    if M == "CCC":
+        print("Canadian Computing Competition")
+        continue
+
+    if M == "CUZ":
+        print("because")
+        continue
+
+    if M == "TY":
+        print("thank-you")
+        continue
+
+    if M == "YW":
+        print("you’re welcome")
+        continue
+
+    print(M)
