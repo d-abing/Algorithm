f445fb92d5c9bfeffbbe53adf5451205deda745f
Dabin Choi <ynlio117@naver.com>
2024-12-27 15:38:28 +0900

    [Bronze IV] Title: Простая задача, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/22155. Простая задача/Простая задача.py
@@ -0,0 +1,8 @@
+n = int(input())
+for _ in range(n):
+    i, f = map(int, input().split())
+    if (i <= 1 and f <= 2) or (i <= 2 and f <= 1):
+        print("Yes")
+    else:
+        print("No")
+
